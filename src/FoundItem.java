934b539eb3e8ed7f72c43d81e99270a177698e55
manasfire1-svg <manas.fire.1@gmail.com>
2026-09-18 21:02:37 +0530

    Add active status to found items

--- src/FoundItem.java
+++ src/FoundItem.java
@@ -38,13 +38,13 @@ public class FoundItem {
                     new FileWriter(file, true))) {
 
                 writer.println(
-                        itemName + "|" +
-                        category + "|" +
-                        description + "|" +
-                        location + "|" +
-                        date + "|" +
-                        contact
-                );
+                itemName + "|" +
+                category + "|" +
+                description + "|" +
+                location + "|" +
+                date + "|" +
+                contact + "|ACTIVE"
+            );
             }
 
             System.out.println();
